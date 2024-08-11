b5c185b1afb5ee1df716e22739de3221d6888dcb
nk0086 <n172586k@gmail.com>
2024-08-12 00:08:40 +0900

    solved 10, 22

--- src/bin/010.rs
+++ src/bin/010.rs
@@ -6,6 +6,29 @@ use proconio::{
 
 #[fastout]
 fn main() {
+    input! {
+        n: usize,
+        cp: [(i64, i64); n],
+        q: usize,
+        lr: [(i64, i64); q],
+    };
 
+    let mut sum_1 = vec![0; n + 1];
+    let mut sum_2 = vec![0; n + 1];
+
+    for i in 0..n {
+        sum_1[i + 1] = sum_1[i];
+        sum_2[i + 1] = sum_2[i];
+        if cp[i].0 == 1 {
+            sum_1[i + 1] += cp[i].1;
+        } else {
+            sum_2[i + 1] += cp[i].1;
+        }
+    }
+
+    for (l, r) in lr {
+        let ans_1 = sum_1[r as usize] - sum_1[l as usize - 1];
+        let ans_2 = sum_2[r as usize] - sum_2[l as usize - 1];
+        println!("{} {}", ans_1, ans_2);
+    }
 }
-
--- src/bin/022.rs
+++ src/bin/022.rs
@@ -6,6 +6,21 @@ use proconio::{
 
 #[fastout]
 fn main() {
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    }
 
+    let g = gcd(a, gcd(b, c));
+    println!("{}", a / g + b / g + c / g - 3);
 }
 
+// gcd
+fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
