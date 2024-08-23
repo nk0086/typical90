848584331f1745418899b4e1b384e0aeb0e8b274
nk0086 <n172586k@gmail.com>
2024-08-23 23:29:43 +0900

    solved

--- src/bin/016.rs
+++ src/bin/016.rs
@@ -1,11 +1,25 @@
 #[allow(unused_imports)]
 use proconio::{
     fastout, input,
-    marker::{Bytes, Chars, Isize1, Usize1},
+    marker::{Bytes, Chars, Isize1},
 };
 
 #[fastout]
 fn main() {
+    input! {
+        n: i64,
+        a: i64, b: i64, c: i64,
+    };
 
+    let mut ans = 10000;
+    for i in 0..10000 {
+        for j in 0..(10000 - i) {
+            let tmp = n - a * i - b * j;
+            if tmp >= 0 && tmp % c == 0 {
+                ans = ans.min(i + j + tmp / c);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
-
